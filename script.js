e3ffab1ae1e8bb6c2426fe52c2f375b2ba37fc4f
francwow <francwow06@gmail.com>
2023-03-02 09:43:28 -0500

    stop debugger

--- script.js
+++ script.js
@@ -1,112 +1,106 @@
 // DARKMODE
-const darkMode = () => { 
-  const darkModeBtn = document.getElementById('darkmode-btn');
+const darkMode = () => {
+  const darkModeBtn = document.getElementById("darkmode-btn");
 
-  darkModeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+  darkModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
   });
 
-  const switches = document.querySelectorAll('.switch');
+  const switches = document.querySelectorAll(".switch");
 
-  switches.forEach(element => {
-    darkModeBtn.addEventListener('click', () => {
-      element.classList.toggle('active');
+  switches.forEach((element) => {
+    darkModeBtn.addEventListener("click", () => {
+      element.classList.toggle("active");
     });
   });
-}
+};
 
 darkMode();
 
-
 // carousel function
 const carousel = () => {
-  const arrows = document.querySelectorAll('.arrow');
-  const carouselItem = document.querySelectorAll('.carousel-item');
+  const arrows = document.querySelectorAll(".arrow");
+  const carouselItem = document.querySelectorAll(".carousel-item");
   let index = 0;
 
   if (index === 0) {
-    arrows[0].classList.add('hide');
-  } else if (index === carouselItem.length -1) {
-    arrows[1].classList.add('hide'); 
+    arrows[0].classList.add("hide");
+  } else if (index === carouselItem.length - 1) {
+    arrows[1].classList.add("hide");
   } else if (index > 0) {
-    arrows[0].classList.remove('hide'); 
-  } else if (index < carouselItem.length -1) {
-    arrows[1].classList.remove('hide'); 
+    arrows[0].classList.remove("hide");
+  } else if (index < carouselItem.length - 1) {
+    arrows[1].classList.remove("hide");
   }
-    
+
   arrows.forEach((arrow, i) => {
-
-    arrow.addEventListener('click', () => {
-
+    arrow.addEventListener("click", () => {
       if (i === 0) {
         index -= 1;
         console.log(index);
         if (index === 0) {
-          arrows[0].classList.add('hide');
-        } else if (index === carouselItem.length -1) {
-          arrows[1].classList.add('hide'); 
-        } 
+          arrows[0].classList.add("hide");
+        } else if (index === carouselItem.length - 1) {
+          arrows[1].classList.add("hide");
+        }
 
-        arrows[1].classList.remove('hide');
+        arrows[1].classList.remove("hide");
         carouselItem.forEach((item, j) => {
           if (j !== index) {
-            item.classList.remove('show-item');
+            item.classList.remove("show-item");
           } else {
-            item.classList.add('show-item');
+            item.classList.add("show-item");
           }
-        });      
-
+        });
       } else if (i === 1) {
         index += 1;
 
         if (index === 0) {
-          arrows[0].classList.add('hide');
-        } else if (index === carouselItem.length -1) {
-          arrows[1].classList.add('hide'); 
-        } 
+          arrows[0].classList.add("hide");
+        } else if (index === carouselItem.length - 1) {
+          arrows[1].classList.add("hide");
+        }
 
-        arrows[0].classList.remove('hide');
+        arrows[0].classList.remove("hide");
         carouselItem.forEach((item, j) => {
           if (j !== index) {
-            item.classList.remove('show-item');
+            item.classList.remove("show-item");
           } else {
-            item.classList.add('show-item');
+            item.classList.add("show-item");
           }
         });
       }
       return index;
     });
   });
-}
+};
 
 carousel();
-
 
 // SUMRANGE JAVASCRIPT EXERCISE.
 const sumRange = () => {
   let array = [];
   function range(arr) {
-    let lowNumber = Math.min(arr[0], arr[1]); 
+    let lowNumber = Math.min(arr[0], arr[1]);
     let highNumber = Math.max(arr[0], arr[1]);
 
     for (i = lowNumber; i <= highNumber; i++) {
       array.push(i);
     }
     return array;
-
   }
 
   function sum(array) {
     let total = 0;
 
     for (i = 0; i < array.length; i++) {
-      total = total+array[i];
+      total = total + array[i];
     }
     return total;
   }
 
   console.log(sum(range([1, 10])));
-}
+};
 
 sumRange();
 
@@ -122,24 +116,20 @@ function factorial(n) {
 console.log(factorial(8));
 // → 40320
 
-
-
 // // Random color While loop
 // let randomColor = Math.floor(Math.random()*16777215).toString(16);
 // let blue = '00b1de';
 
 // while (randomColor !== blue) {
-//   console.log('not blue');    
+//   console.log('not blue');
 //   randomColor = Math.floor(Math.random()*16777215).toString(16);
-// } 
-
-
+// }
 
 // chapter 2 exercises - looping a triangle
-let hashTag = '';
+let hashTag = "";
 
 for (let i = 0; i < 7; i++) {
-  hashTag += '#';
+  hashTag += "#";
   console.log(hashTag);
 }
 
@@ -149,52 +139,116 @@ let numberCount = 0;
 for (let i = 0; i < 100; i++) {
   numberCount += 1;
   if (numberCount % 3 === 0 && numberCount % 5 == 0) {
-    console.log('FizzBuzz');
+    console.log("FizzBuzz");
   } else if (numberCount % 3 === 0) {
-    console.log('Fizz');
+    console.log("Fizz");
   } else if (numberCount % 5 === 0) {
-    console.log('Buzz');
+    console.log("Buzz");
   } else {
     console.log(numberCount);
   }
 }
 
 // chapter 2 exercises - ChessBoard
-let board =   ' ' + '#' + ' ' + '#' + ' ' + '#' + ' ' + '#' + '\n' 
-            + '#' + ' ' + '#' + ' ' + '#' + ' ' + '#' + ' ' + '\n' 
-            + ' ' + '#' + ' ' + '#' + ' ' + '#' + ' ' + '#' + '\n' 
-            + '#' + ' ' + '#' + ' ' + '#' + ' ' + '#' + ' ' + '\n'
-            + ' ' + '#' + ' ' + '#' + ' ' + '#' + ' ' + '#' + '\n' 
-            + '#' + ' ' + '#' + ' ' + '#' + ' ' + '#' + ' ' + '\n' 
-            + ' ' + '#' + ' ' + '#' + ' ' + '#' + ' ' + '#' + '\n' 
-            + '#' + ' ' + '#' + ' ' + '#' + ' ' + '#' + ' ' + '\n';
+let board =
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  "\n" +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "\n" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  "\n" +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "\n" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  "\n" +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "\n" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  "\n" +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "#" +
+  " " +
+  "\n";
 
-let chessBoard = '';
+let chessBoard = "";
 
 let size = 8;
 
 for (let i = 1; i <= size; i++) {
-
   if (i % 2 === 0) {
     for (j = 1; j <= size; j++) {
       if (j % 2 === 0) {
-        chessBoard += ' ';
+        chessBoard += " ";
       } else {
-        chessBoard += '#';
+        chessBoard += "#";
       }
       if (j === size) {
-        chessBoard += '\n';
+        chessBoard += "\n";
       }
-    }    
+    }
   } else {
     for (j = 1; j <= size; j++) {
       if (j % 2 === 0) {
-        chessBoard += '#';
+        chessBoard += "#";
       } else {
-        chessBoard += ' ';
+        chessBoard += " ";
       }
       if (j === size) {
-        chessBoard += '\n';
+        chessBoard += "\n";
       }
     }
   }
@@ -211,9 +265,9 @@ const min = (a, b) => {
   } else if (b > a) {
     return a;
   } else {
-    return 'both numbers are the same';
+    return "both numbers are the same";
   }
-}
+};
 
 console.log(min(0, 10));
 // → 0
@@ -231,7 +285,7 @@ const isEven = (a) => {
   } else {
     return isEven(a - 2);
   }
-}
+};
 
 console.log(isEven(50));
 // → true
@@ -244,22 +298,22 @@ console.log(isEven(-1));
 const countBs = (a) => {
   let upperCaseB = 0;
   for (let i = 0; i < a.length; i++)
-    if (a[i].toUpperCase() === 'B') {
+    if (a[i].toUpperCase() === "B") {
       upperCaseB += 1;
     }
-  return upperCaseB;  
-}
+  return upperCaseB;
+};
 
 const countChar = (a, b) => {
   let charNum = 0;
   for (let i = 0; i < a.length; i++) {
-    if (a[i] === 'k') {
+    if (a[i] === "k") {
       charNum += 1;
     }
   }
   let charString = "There are " + charNum + " " + b + " characters.";
   return charString;
-}
+};
 
 console.log(countBs("BBC"));
 // → 2
@@ -268,7 +322,6 @@ console.log(countChar("kakkerlak", "k"));
 
 // chapter 4 exercises - the sum of a range
 function range(start, end, step = start < end ? 1 : -1) {
-
   // this condition does exactly the same as the one above next to the 'step' parameter.
   // if (step !== undefined) {
   //     step === step;
@@ -277,7 +330,6 @@ function range(start, end, step = start < end ? 1 : -1) {
   // } else if (start > end) {
   //     step = -1;
   // }
-
 
   let array = [];
 
@@ -307,21 +359,18 @@ function reverseArray(array) {
 
   // this method lets us create a new copy of the array without modifying the existing one
   for (let i = array.length - 1; i >= 0; i--) {
-    array2.push(array[i]);  
-    }
+    array2.push(array[i]);
+  }
 
   return array2;
 }
 
 function reverseArrayInPlace(array) {
-
-  for (let i = 0; i < Math.floor(array.length/2); i++) {
-
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
     // we are creating a copy of the value to reasign it later
     let old = array[i];
     array[i] = array[array.length - 1 - i];
     array[array.length - 1 - i] = old;
-
   }
 
   return array;
@@ -331,7 +380,6 @@ let arrayValue = [7, 2, 3, 4, 5, 6, 7];
 let arrayLetters = ["A", "B", "C", "D", "E", "F"];
 let arrayCopy = arrayLetters.slice(0);
 
-
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 console.log(arrayLetters);
@@ -340,26 +388,23 @@ console.log(reverseArray(arrayLetters));
 
 // chapter 4 exercises - a list
 function arrayToList(array) {
-
   // we create two empty objects that represent the outer most object and the one that populates it from within
   let list = {};
   let rest = {};
 
   // inverse for loop to start creating objects from the end of the array
-  for (let i = array.length -1; i >= 0 ; i--) {
-
+  for (let i = array.length - 1; i >= 0; i--) {
     // create property-value pairs for the rest object dinamically with a for loop
     rest.value = array[i];
     // if the index is equal to the end of the array, set the rest property equal to null
-    if (i === array.length -1) {
+    if (i === array.length - 1) {
       rest.rest = null;
-    // if the index is not at the end of the array, set the rest property to be equal to whatever the list object is equal to at that time  
+      // if the index is not at the end of the array, set the rest property to be equal to whatever the list object is equal to at that time
     } else {
       rest.rest = list;
     }
     // insert the items within the rest object at the end of the loop into the list object
-    list = {...rest};
-
+    list = { ...rest };
   }
 
   return list;
@@ -368,14 +413,12 @@ function arrayToList(array) {
 console.log(arrayToList([10, 20, 30]));
 // → {value: 10, rest: {value: 20, rest: {value: 30, rest: null}}}
 
-
 function listToArray(list) {
-
   // set a new list to be equal to the one given as argument and create an empty array to be populated with the values from that list
   let listTwo = list;
   const array = [];
 
-  // create a while statement to loop through the entirety of the given list 
+  // create a while statement to loop through the entirety of the given list
   while (true) {
     // push into the new array the value from the new list
     array.push(listTwo.value);
@@ -387,28 +430,25 @@ function listToArray(list) {
     // and the loop starts over again with the new list updated so it can keep looking for new values within the list
     listTwo = listTwo.rest;
   }
-  
+
   return array;
 }
 
-console.log(listToArray(arrayToList([10, 20, 30])))
+console.log(listToArray(arrayToList([10, 20, 30])));
 // -> [10, 20, 30]
 
-
 function prepend(e, list) {
-
   // We create a newList object that will contain value and rest properties, we set value equal to e and rest equal to list
-  let newList = {value: e, rest: list}
+  let newList = { value: e, rest: list };
 
   // Return the new object
   return newList;
 }
 
-// The console.log method first uses the deepest function call to crerate an object, 
+// The console.log method first uses the deepest function call to crerate an object,
 // then proceeds with the next until it reaches the outer most function call.
 console.log(prepend(10, prepend(20, prepend(30, null))));
 // → {value: 10, rest: {value: 20, rest: {value: 30, rest: null}}}
-
 
 function nth(list, number) {
   // list = {value: 10, rest: {value: 20, rest: {value: 30, rest: null}}}
@@ -426,7 +466,7 @@ function nth(list, number) {
 
   // We use the same method from the listToArray(list) function to populate the new array
   while (true) {
-    item.push(newList.value)
+    item.push(newList.value);
     if (newList.rest === null) {
       break;
     }
@@ -448,63 +488,58 @@ function nth(list, number) {
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
 
-
 // chapter 4 exercises - deep comparison
 function deepEqual(a, b) {
   // console.log(typeof a);
   // console.log(b.a);
-  debugger;
 
   // if a & b are null return true
   if (a === null || b === null) {
     return false;
-  } else if (typeof a === 'object' && typeof b === 'object') {
-      if (Array.isArray(a) && Array.isArray(b)) {
-        if (a.toString() !== b.toString()) {
+  } else if (typeof a === "object" && typeof b === "object") {
+    if (Array.isArray(a) && Array.isArray(b)) {
+      if (a.toString() !== b.toString()) {
+        return false;
+      }
+    } else {
+      let array1 = Object.keys(a);
+      let array2 = Object.keys(b);
+
+      if (array1.length === array2.length) {
+        if (array1.toString() !== array2.toString()) {
           return false;
-        }
-      } else {
-        let array1 = Object.keys(a);
-        let array2 = Object.keys(b);
+        } else {
+          // improve for loop with .includes
+          for (let i = 0; i < array1.length; i++) {
+            for (let j = 0; j < array2.length; j++) {
+              if (array1[i] === array2[j]) {
+                // console.log(array1[i])
+                // console.log(array2[j])
+                console.log(a[array1[i]]);
+                console.log(b[array2[j]]);
 
-        if (array1.length === array2.length) {
-          if (array1.toString() !== array2.toString()) {
-            return false;
-          } else {
+                let arr1 = [a[array1[i]]];
+                let arr2 = [b[array2[j]]];
 
-            // improve for loop with .includes
-            for (let i = 0; i < array1.length; i++) {
-              for (let j = 0; j < array2.length; j++) {
-                if (array1[i] === array2[j]) {
-                  // console.log(array1[i])
-                  // console.log(array2[j])
-                  console.log(a[array1[i]]);
-                  console.log(b[array2[j]]);
-
-                  let arr1 = [a[array1[i]]];
-                  let arr2 = [b[array2[j]]];
-
-                  if (arr1.toString() !== arr2.toString()) {
+                if (arr1.toString() !== arr2.toString()) {
+                  return false;
+                } else {
+                  let isDeepEqual = deepEqual(a[array1[i]], b[array2[j]]);
+                  if (!isDeepEqual) {
                     return false;
-                  } else {
-                    let isDeepEqual = deepEqual(a[array1[i]], b[array2[j]]);
-                    if (!isDeepEqual) {
-                      return false;
-                    }
                   }
-                } 
+                }
               }
             }
-
-
           }
-        } else {
-              return false;
         }
+      } else {
+        return false;
       }
+    }
   } else if (a !== b) {
     return false;
-  } 
+  }
 
   return true;
 }
@@ -514,45 +549,43 @@ const object1 = {
   m: 10,
   c: {
     d: {
-      f: true
-    }
+      f: true,
+    },
   },
-  a: 'somestring',
-  b: 42
+  a: "somestring",
+  b: 42,
 };
 
 const object2 = {
   m: 10,
   c: {
     d: {
-      f: false
-    }
+      f: false,
+    },
   },
-  a: 'somestring',
-  b: 42
+  a: "somestring",
+  b: 42,
 };
 
 const object3 = {
-  d: 'somestring',
+  d: "somestring",
   b: 42,
   c: false,
-  f: 10
+  f: 10,
 };
 
-const arrayExample = ['a', 'b', 'c'];
+const arrayExample = ["a", "b", "c"];
 
 let numberOne = 1;
 
-let numberTwo = 'two';
+let numberTwo = "two";
 
 // console.log(deepEqual(object1, object2));
 
-let obj = {here: {is: "an"}, object: 2};
+let obj = { here: { is: "an" }, object: 2 };
 console.log(deepEqual(obj, obj));
 // // → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, { here: 1, object: 2 }));
 // // → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
 // → true
-
-
